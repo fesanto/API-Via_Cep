7e84532e8a70f0a1758d8d0d2a73e573d34d81cc
Hellen Brunelli <hellenbrunelli01@gmail.com>
2019-06-28 10:18:20 -0300

    add initial archives

---
+++ index.js
@@ -0,0 +1,9 @@
+const data = {
+    "cep": "00000000",
+    "logradouro": "Logradouro",
+    "complemento": "Complemento",
+    "bairro": "Bairro",
+    "localidade": "Cidade"
+};
+
+console.log(data.bairro);
